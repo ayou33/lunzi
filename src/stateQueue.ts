bfda257ee7f4ef98722d53ef5aad473b2e1ffc35
阿佑 <ayooooo@petalmail.com>
2024-04-03 22:58:32 +0800

    add test cases

--- src/stateQueue.ts
+++ src/stateQueue.ts
@@ -5,50 +5,92 @@
  */
 import useEvent from './event'
 
+/**
+ * Enum for queue states.
+ * @readonly
+ * @enum {number}
+ */
 export enum QueueState {
   IDLE,
   BUSY,
   INTERRUPT,
 }
 
-export interface Task {
-  id: string;
+/**
+ * Interface for tasks.
+ * @interface
+ */
+export interface TaskMeta {
   priority?: number;
   label?: string;
-  run: <T>() => Promise<T>;
+  run: () => unknown;
 }
 
-type RequiredTask = Required<Task>
+export enum TaskRunType {
+  MANUAL,
+  AUTO,
+  IMMEDIATE,
+}
+
+type Task = Required<Omit<TaskMeta, 'run'>> & {
+  id: string;
+  run: () => Promise<unknown>;
+}
 
 const DEFAULT_PRIORITY = 0
 
 const LABEL_UNKNOWN = 'unknown'
 
-function makeId () {
+export interface StateQueue {
+  enqueue: (task: TaskMeta | TaskMeta['run'], runType?: TaskRunType) => string;
+  cancel: (idOrLabel: string | string[]) => void;
+  getTasks: () => Task[];
+  getRunningTasks: () => Task[];
+  on: (state: QueueState, handler: () => void, oneOff?: boolean) => Function;
+}
+
+/**
+ * Function to generate a random ID.
+ * @returns {string} A random string.
+ */
+function makeId (): string {
   return Math.random().toString(36).slice(2)
 }
 
-export default function stateQueue (parallel = 1) {
-  const tasks: RequiredTask[] = []
-  const running: RequiredTask[] = []
-  
+/**
+ * Function to manage a state queue.
+ * @param {number} parallel - The number of tasks that can run in parallel.
+ * @returns {Object} The state queue object.
+ */
+export default function stateQueue (parallel: number = 1): StateQueue {
+  const tasks: Task[] = []
+  const running: Task[] = []
+
   const { on, once, emit } = useEvent()
-  
+
+  /**
+   * Function to manage the execution of tasks.
+   */
   function next () {
-    if (tasks.length === 0) {
+    if (tasks.length === 0 && running.length === 0) {
       idle()
     }
-    
+
     if (tasks.length > 0 && running.length < parallel) {
       const task = tasks.shift()
-      
+
       if (task) {
         run(task)
       }
     }
   }
-  
-  function buildTaskObject (task: Parameters<typeof enqueue>[0]): RequiredTask {
+
+  /**
+   * Function to build a task object.
+   * @param {Object} task - The task to be built.
+   * @returns {Object} The built task object.
+   */
+  function buildTaskObject (task: Parameters<typeof enqueue>[0]): Task {
     const run = 'function' === typeof task ? task : task.run
     
     return {
@@ -56,82 +98,115 @@ export default function stateQueue (parallel = 1) {
       priority: DEFAULT_PRIORITY,
       label: LABEL_UNKNOWN,
       ...task,
-      async run<T> () {
-        const result = await run<T>()
+      async run () {
+        const result = await run()
         interrupt()
         return result
       },
     }
   }
-  
-  function run (task: RequiredTask) {
+
+  /**
+   * Function to run a task.
+   * @param {Object} task - The task to be run.
+   */
+  function run (task: Task) {
     running.push(task)
-    
+
     busy()
-    
+
     task
       .run()
       .finally(() => {
         running.splice(running.indexOf(task), 1)
-        
+
         next()
       })
   }
-  
-  function enqueue (task: Partial<Task> & Pick<Task, 'run'> | Task['run'], ir = false) {
+
+  /**
+   * Function to enqueue a task.
+   * @param {Object} task - The task to be enqueued.
+   * @param runType
+   * @returns {string} The ID of the enqueued task.
+   */
+  function enqueue (task: TaskMeta | TaskMeta['run'], runType: TaskRunType = TaskRunType.AUTO): string {
     const taskObject = buildTaskObject(task)
-    
-    if (ir) {
+
+    if (runType === TaskRunType.IMMEDIATE) {
       run(taskObject)
     } else {
       const position = tasks.findIndex(({ priority }) => priority! < taskObject.priority!)
       tasks.splice(position === -1 ? tasks.length : position, 0, taskObject)
+      
+      if (runType === TaskRunType.AUTO) next()
     }
-    
-    next()
-    
+
     return taskObject.id
   }
-  
+
+  /**
+   * Function to emit an interrupt event.
+   */
   function interrupt () {
     emit(stateEvent(QueueState.INTERRUPT))
   }
-  
+
+  /**
+   * Function to emit an idle event.
+   */
   function idle () {
     emit(stateEvent(QueueState.IDLE))
   }
-  
+
+  /**
+   * Function to emit a busy event.
+   */
   function busy () {
     emit(stateEvent(QueueState.BUSY))
   }
-  
-  function stateEvent (state: QueueState) {
+
+  /**
+   * Function to create a state event.
+   * @param {QueueState} state - The state of the event.
+   * @returns {string} The state event string.
+   */
+  function stateEvent (state: QueueState): string {
     return `state:${state}`
   }
-  
-  function onStateChange (state: QueueState, handler: (tasks: Task[]) => void, oneOff = false) {
-    return (oneOff ? once : on)(stateEvent(state), () => handler(tasks.flat()))
+
+  /**
+   * Function to handle state changes.
+   * @param {QueueState} state - The state to listen for.
+   * @param {Function} handler - The function to execute when the state changes.
+   * @param {boolean} oneOff - Flag to indicate if the handler should be executed only once.
+   * @returns {Function} The function to unsubscribe.
+   */
+  function onStateChange (state: QueueState, handler: () => void, oneOff: boolean = false): Function {
+    return (oneOff ? once : on)(stateEvent(state), handler)
   }
-  
-  function cancel (id: string | string[]): void
-  
-  function cancel (label: string | string[]): void
-  
-  function cancel (idOrLabel: string | string[]): void {
+
+  /**
+   * Function to cancel a task.
+   * @param {string|string[]} idOrLabel - The ID or label of the task to be cancelled.
+   */
+  function cancel (idOrLabel: string | string[]) {
     const predicate = 'string' === typeof idOrLabel
-      ? ({ id, label }: Pick<RequiredTask, 'id' | 'label'>) => !(id == idOrLabel || label === idOrLabel)
-      : ({ id, label }: Pick<RequiredTask, 'id' | 'label'>) => !(idOrLabel.includes(id) || idOrLabel.includes(label))
-    
+      ? ({ id, label }: Pick<Task, 'id' | 'label'>) => !(id == idOrLabel || label === idOrLabel)
+      : ({ id, label }: Pick<Task, 'id' | 'label'>) => !(idOrLabel.includes(id) || idOrLabel.includes(label))
+
     const left = tasks.filter(predicate)
-    
+
     tasks.length = 0
-    
+
     tasks.push(...left)
   }
-  
+
   return {
     enqueue,
     on: onStateChange,
     cancel,
+    getTasks: () => tasks,
+    getRunningTasks: () => running,
   }
 }
--- test/stateQueue.test.ts
+++ test/stateQueue.test.ts
@@ -3,15 +3,54 @@
  * Author: 阿佑[ayooooo@petalmail.com]
  * Date: 2024/4/3 16:55
  */
-import StateQueue from '../src/stateQueue'
+import stateQueue, { QueueState, TaskMeta, TaskRunType } from '../src/stateQueue'
 
 describe('stateQueue', () => {
-  const fn = (state: number) => state + 1
-
-  test('状态队列', () => {
-    const state = 1
-    const result = fn(state)
-
-    expect(StateQueue(fn)(state)).toBe(result)
+  let queue: ReturnType<typeof stateQueue>
+  let task: TaskMeta
+  
+  beforeEach(() => {
+    queue = stateQueue()
+    task = {
+      label: 'testTask',
+      run: jest.fn().mockResolvedValue('test'),
+    }
+  })
+  
+  test('enqueue adds task to the queue', () => {
+    const id = queue.enqueue(task, TaskRunType.MANUAL)
+    expect(queue.getTasks()).toContainEqual(expect.objectContaining({ id }))
+  })
+  
+  test('enqueue runs task immediately if ir flag is true', async () => {
+    queue.enqueue(task, TaskRunType.IMMEDIATE)
+    expect(task.run).toHaveBeenCalled()
+  })
+  
+  test('cancel removes task from the queue by id', () => {
+    const id = queue.enqueue(task)
+    queue.cancel(id)
+    expect(queue.getTasks()).not.toContain(task)
+  })
+  
+  test('cancel removes task from the queue by label', () => {
+    task.label = 'testLabel'
+    queue.enqueue(task)
+    queue.cancel(task.label)
+    expect(queue.getTasks()).not.toContain(task)
+  })
+  
+  test('onStateChange triggers handler when state changes', () => {
+    const handler = jest.fn()
+    queue.on(QueueState.BUSY, handler)
+    queue.enqueue(task)
+    expect(handler).toHaveBeenCalled()
+  })
+  
+  test('onStateChange does not trigger handler when state does not change', () => {
+    const handler = jest.fn()
+    queue.on(QueueState.IDLE, handler)
+    queue.cancel(task.label!)
+    expect(handler).not.toHaveBeenCalled()
   })
 })
